2c6f1b75520fd4aa3fb4c1c2c7d0240709cc4ffe
arduinodenis <90153974+ArduinoDenis@users.noreply.github.com>
2024-02-18 17:35:40 +0100

    Update arduino_ricevitore_ir.ino

--- arduino_ricevitore_ir.ino
+++ arduino_ricevitore_ir.ino
@@ -1,20 +1,18 @@
-#include <IRremote.h> // use the library
-int receiver = 11; // pin 1 of IR receiver to Arduino digital pin 11
-IRrecv irrecv(receiver); // create instance of 'irrecv'
-decode_results results;
+#include <IRremote.h> // Include the IRremote library
 
-void setup()
-{
-  Serial.begin(9600); // for serial monitor output
-  irrecv.enableIRIn(); // Start the receiver
-  pinMode(9, OUTPUT); // Pin 9 output
+int receiver = 11; // IR receiver pin connected to Arduino digital pin 11
+IRrecv irrecv(receiver); // Create an instance of 'irrecv' for receiving IR signals
+decode_results results; // Variable to store the decoded IR results
+
+void setup() {
+  Serial.begin(9600); // Initialize serial communication for serial monitor output
+  irrecv.enableIRIn(); // Start the IR receiver
+  pinMode(9, OUTPUT); // Set pin 9 as output for controlling an LED
 }
-void loop()
-{
-  if (irrecv.decode(&results)) // have we received an IR signal?
-  {
-    Serial.println(results.value, HEX); // display it on serial monitor in hexadecimal
-    irrecv.resume();// receive the next value
+
+void loop() {
+  if (irrecv.decode(&results)) { // Check if we have received an IR signal
+    Serial.println(results.value, HEX); // Display the received signal value in hexadecimal on the serial monitor
+    irrecv.resume(); // Continue to receive the next IR signal
   }
-   
 }
