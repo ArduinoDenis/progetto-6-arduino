c4e4542968baa42d575932a8ab6a9cd9d8145ea5
arduinodenis <90153974+ArduinoDenis@users.noreply.github.com>
2024-02-18 17:34:36 +0100

    Update arduino_trasmettitore_ir.ino

--- arduino_trasmettitore_ir.ino
+++ arduino_trasmettitore_ir.ino
@@ -1,26 +1,27 @@
-#include <IRremote.h> // use the library
-int receiver = 11; // pin 1 of IR receiver to Arduino digital pin 11
-IRrecv irrecv(receiver); // create instance of 'irrecv'
-decode_results results;
+#include <IRremote.h> // Include the IRremote library
 
-void setup()
-{
-  Serial.begin(9600); // for serial monitor output
-  irrecv.enableIRIn(); // Start the receiver
-  pinMode(9, OUTPUT); // Pin 9 output
+int receiver = 11; // IR receiver pin connected to Arduino digital pin 11
+IRrecv irrecv(receiver); // Create an instance of 'irrecv' for receiving IR signals
+decode_results results; // Variable to store the decoded IR results
+
+void setup() {
+  Serial.begin(9600); // Initialize serial communication for serial monitor output
+  irrecv.enableIRIn(); // Start the IR receiver
+  pinMode(9, OUTPUT); // Set pin 9 as output for controlling an LED
 }
-void loop()
-{
-  if (irrecv.decode(&results)) // have we received an IR signal?
-  {
-    Serial.println(results.value, HEX); // display it on serial monitor in hexadecimal
-    irrecv.resume();// receive the next value
+
+void loop() {
+  if (irrecv.decode(&results)) { // Check if we have received an IR signal
+    Serial.println(results.value, HEX); // Display the received signal value in hexadecimal on the serial monitor
+    irrecv.resume(); // Continue to receive the next IR signal
   }
  
-  if ( results.value == 0x406 ||  results.value == 0x10406 ){ //tasto 6 sul telecomando
-    digitalWrite(9, HIGH);   // set the LED on
+  // Check if the received signal matches the button 6 on the remote control
+  if (results.value == 0x406 || results.value == 0x10406) {
+    digitalWrite(9, HIGH); // Turn on the LED connected to pin 9
   } 
- if ( results.value == 0x404 ||  results.value == 0x10404 ){ //tasto 4 sul telecomando
-    digitalWrite(9, LOW);   // set the LED 0ff
+  // Check if the received signal matches the button 4 on the remote control
+  if (results.value == 0x404 || results.value == 0x10404) {
+    digitalWrite(9, LOW); // Turn off the LED connected to pin 9
   }  
 }
